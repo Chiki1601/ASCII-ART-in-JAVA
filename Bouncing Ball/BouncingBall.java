46ece56b37445b521c104f8099cf68334d8a8238
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-10 12:56:46 +0530

    Create BouncingBall.java

---
+++ Bouncing Ball/BouncingBall.java
@@ -0,0 +1,105 @@
+public class BouncingBall {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		while (true) {
+			System.out.println(" O ");
+			System.out.println("   ");
+			System.out.println("___");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println(" | ");
+			System.out.println(" O ");
+			System.out.println("___");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("  ");
+			System.out.println("   ");
+			System.out.println("_O_");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("   ");
+			System.out.println("   ");
+			System.out.println("_<>_");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("  ");
+			System.out.println("   ");
+			System.out.println("_O_");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+
+			System.out.println("  ");
+			System.out.println(" O ");
+			System.out.println("_|_");
+
+			Thread.sleep(100);
+
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+		}
+
+	}
+
+}
