99816723f73da3b0579e1ec30188e65396b68b53
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-28 16:38:52 +0530

    Create dog.java

---
+++ Dog/dog.java
@@ -0,0 +1,31 @@
+public class Program
+{
+	public static void main(String[] args) {
+System.out.println("\t\t\t\t     __\t  ");
+	System.out.println("\t\t\t\t   .b$$b\t ");
+    	System.out.println("\t\t\t\t .\' TO$;\\\t");
+        	System.out.println("\t\t\t\t/  : TP._;    ");
+	System.out.println("\t\t\t       / _.;  :Tb|    ");
+    	System.out.println("\t\t\t      /   /   ;j$j    ");
+System.out.println("\t\t          _.-\"\t      $$$$    ");
+	System.out.println("\t\t       .' ..\t     d$$$$;    ");
+	System.out.println("\t\t      /  /P'\t    d$$$$P. |\\ ");
+	System.out.println("\t\t     /   \" \t  .d$$$P' |\\^^l");
+        	System.out.println("\t\t   .'       \t  'T$P^^^^^^  :");
+    	System.out.println("\t       ._.'       _.'\t              ;");
+        	System.out.println("\t    '-.-\".-'-'  ._.\t    _.-'    .-'");
+    	System.out.println("\t  '.-^______  ._     \t        .-'");
+System.out.println("\t -(.g$$$$$$$b.       \t      .'");
+System.out.println("\t   \"\"^^T$$$P^)     \t   .(:");
+System.out.println("\t     _/  -\"  /.'    \t  /:/;");
+    	System.out.println("\t  ._.'-''-'  \")/    \t  /:/;");
+    	System.out.println("       '-.-\"..--\"\"   \" /\t  /  ;");
+        	System.out.println("      .-\" ..--\"         -'\t     ;");
+    	System.out.println("      ..--\"\"--.-\"         (\\\t   .-(\\");
+System.out.println("        ..--\"\"               '-\\(\\/;'");
+        	System.out.println("          _.                       :");
+System.out.println("                                   ;'-");
+    	System.out.println("                                  :\\");
+	            System.out.println("                Pooja Patel       ;     Class of 2018");
+		}
+}
