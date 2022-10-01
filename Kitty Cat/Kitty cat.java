83a3009e089be4505494349d8f2fb7ae8a5c1ec5
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-01 09:32:49 +0530

    Create Kitty cat.java

---
+++ Kitty Cat/Kitty cat.java
@@ -0,0 +1,31 @@
+public class Program
+{
+    public static void main(String[] args) {
+        //#Kitty teddy
+
+
+System.out.println("´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´");
+System.out.println(" ´´´´´´¶¶´´´´¶¶¶¶¶´´¶¶¶¶´¶¶¶¶´´");
+System.out.println(" ´´´´´´¶´´´´´´´´´´¶¶¶¶´¶¶´´´´¶´");
+System.out.println(" ´´´´´´¶´´´´´´´´´´¶´¶¶¶¶¶¶´´´¶´");
+System.out.println(" ´´´´´¶´´´´´´´´´´¶¶¶¶¶´´´¶¶¶¶¶´");
+System.out.println("´´´´¶´´´´´´´´´´´´´´´´¶¶¶¶¶¶¶¶´");
+System.out.println(" ´´´¶´´´´´´´´´´´´´´´´´´´¶¶¶¶¶´´");
+System.out.println(" ´¶¶¶´´´´´¶´´´´´´´´´´´´´´´´´¶´´");
+System.out.println("´´´¶´´´´¶¶´´´´´´´´´´´´´´´´´¶´´");
+System.out.println(" ´´´¶¶´´´´´´´´´´´´´´´´¶¶´´´´¶´´");
+System.out.println(" ´´¶¶¶´´´´´´´´´¶¶¶´´´´¶¶´´´¶¶´´");
+System.out.println(" ´´´´´¶¶´´´´´´´´´´´´´´´´´´¶¶¶´´");
+System.out.println(" ´´´´´´´¶¶¶´´´´´´´´´´´´´¶¶¶´´´´");
+System.out.println(" ´´´¶¶¶¶¶´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´");
+System.out.println(" ´´´¶´´´´¶¶¶¶¶´´´´¶¶¶¶´´´¶´´´´´");
+System.out.println(" ´´´¶´´´´¶¶¶´¶¶¶¶¶¶¶¶´´´¶¶¶´´´´");
+System.out.println("´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´¶¶¶¶¶´´´¶¶´´");
+System.out.println(" ´´¶´´´´´´¶¶¶¶¶¶¶¶¶¶¶´´´´´´´¶´´");
+System.out.println("´¶´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´¶´´");
+System.out.println("´´¶´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´¶´´");
+System.out.println("´´¶¶´´´´´´´¶¶´´´´¶¶´´´´´´¶¶´´´");
+System.out.println("´´´´¶¶¶¶¶¶¶´´´´´´´´¶¶¶¶¶¶´´´´´");
+		
+	}
+}
