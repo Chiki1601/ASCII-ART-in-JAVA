0fb2d4945b303be845013e78d07ec8316e4bb09c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-01 09:28:50 +0530

    Create Message.java

---
+++ Quotes + messages/Message.java
@@ -0,0 +1,30 @@
+public class Program
+{
+    public static void main(String[] args) {
+        //Proposal
+
+
+
+System.out.println("█ █ █ █ █ █ █ █ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█");
+System.out.println("█●̮̑Loving hundred wrong personsmay not even effect your life,");
+System.out.println("█●̮̑");
+System.out.println("█●̮̑but hating one right personwill leave you a brokenthroughout your life. .!");
+System.out.println("█");
+System.out.println("█ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ █ █ █ █ █ █ ●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+System.out.println("█ █ █ █ █ █ █ █ █ █ █ █ █ █ █●̮̑?¸¸.•´¯¯¯¯`•.¸¸?̮̑●̮̑?");
+	}
+}
