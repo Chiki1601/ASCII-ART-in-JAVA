b70fe03cb540672f57d618a6e5456f0bb5d3f856
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-02 12:53:05 +0530

    Create sparrow.java

---
+++ Sparrow/sparrow.java
@@ -0,0 +1,28 @@
+public class Program
+{
+    public static void main(String[] args) {
+		
+System.out.println("______________¶¶¶¶¶¶¶¶");
+System.out.println("_¶¶¶__________¶¶¶¶_¶¶");
+System.out.println("_¶¶_¶¶¶_________¶¶¶___¶¶");
+System.out.println("_¶__¶¶¶¶_______¶¶____¶¶");
+System.out.println("_¶___¶¶¶¶_____¶¶____¶¶");
+System.out.println("_¶¶___¶¶¶¶____¶¶___¶¶");
+System.out.println("_¶¶____¶¶¶¶¶¶__¶____¶¶");
+System.out.println("_¶¶______¶¶¶_¶¶____¶¶");
+System.out.println("_¶_______¶¶_¶___¶¶¶¶¶¶¶");
+System.out.println("_¶¶_______¶¶¶__¶¶_ö_¶¶¶");
+System.out.println("_¶¶_______¶¶¶¶¶¶¶____¶¶¶¶");
+System.out.println("__¶¶___________¶¶¶");
+System.out.println("__¶¶¶___________¶¶");
+System.out.println("___¶¶¶¶________¶¶");
+System.out.println("_____¶¶_______¶¶");
+System.out.println("_____¶¶¶¶¶¶______¶¶");
+System.out.println("¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_______¶¶");
+System.out.println("_¶___________¶¶¶¶");
+System.out.println("_¶¶________¶¶¶¶¶");
+System.out.println("_¶¶______¶¶¶¶¶¶¶¶¶");
+System.out.println("__¶¶¶¶_____¶¶");
+System.out.println("___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+	}
+}
