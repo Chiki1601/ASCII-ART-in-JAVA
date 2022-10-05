667a68e7e436876622fae36a4138a3a52ab7979b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-05 13:00:18 +0530

    Create Church.java

---
+++ Church/Church.java
@@ -0,0 +1,52 @@
+public class Program
+{
+    public static void main(String[] args) {
+		//Church
+
+
+                          
+System.out.println("═══════════█");
+System.out.println("══════════███");
+System.out.println("═══════════█");
+System.out.println("═══════════█");
+System.out.println("══════════█▓█");
+System.out.println("══════════█▒█");
+System.out.println("═════════█████");
+System.out.println("═══════██▒▒▒▒▒██");
+System.out.println("═════██▒▒▒▒▒▒▒▒▒██");
+System.out.println("════██▒▒▒▒▒▒▒▒▒▒▒██");
+System.out.println("═══██▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+System.out.println("═██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+System.out.println("═█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+System.out.println("██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+System.out.println("█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█");
+System.out.println("██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██");
+System.out.println("═██████████████████████");
+System.out.println("══███▒▒▒▒▒▒▒▒▒▒▒▒▒████");
+System.out.println("═█▒▒▒████▒▒██████▒▒▒█");
+System.out.println("═█▒▒▒█──█▒▒██████▒▒▒█");
+System.out.println("═█▒▒▒████▒▒██┼┼██▒▒▒█");
+System.out.println("=█▒▒▒████▒▒██┼┼██▒▒▒█");
+System.out.println("═█▒▒▒████▒▒██████▒▒▒█");
+System.out.println("██▒▒▒████▒▒██████▒▒▒████");
+System.out.println("██▒▒▒████▒▒██████▒▒▒████");
+System.out.println("██▒▒▒████▒▒██████▒▒▒████");
+System.out.println("██▒▒▒████▒▒▒▒▒▒▒▒▒▒▒████");
+System.out.println("██▒▒▒████▒▒▒▒▒▒▒▒▒▒▒████");
+System.out.println("██▒▒▒████▒▒▒▒▒▒▒▒▒▒▒████");
+System.out.println("██▒▒▒████▒▒▒▒▒▒▒▒▒▒▒████");
+System.out.println("██▒▒▒████▒▒▒▒▒▒▒▒▒▒▒████");
+System.out.println("█████████┼┼█████████████");
+System.out.println("███████┼┼┼██████████████");
+System.out.println("█████┼┼┼████████████████");
+System.out.println("███┼┼┼██████████████████");
+System.out.println("███┼┼┼██████████████████");
+System.out.println("███┼┼┼██████████████████");
+System.out.println("████┼┼┼┼┼███████████████");
+System.out.println("██████┼┼┼┼┼┼████████████");
+System.out.println("█████████┼┼┼┼┼┼┼┼███████");
+System.out.println("█████████┼┼┼┼┼┼┼┼┼██████");
+System.out.println("███████████┼┼┼┼┼┼┼┼┼┼███");
+System.out.println("████████████████████████");
+	}
+}
