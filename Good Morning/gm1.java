0ae5418506a648fca1fef1475aaa1d1e0b4b4a84
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 11:24:30 +0530

    Create gm1.java

---
+++ Good Morning/gm1.java
@@ -0,0 +1,25 @@
+public class Program
+{
+    public static void main(String[] args) {
+		
+// good morning
+     
+System.out.println("🌷🌹🍃🌹🌷G ");
+System.out.println("┏╮╭┓┏╮╭┓ O ");
+System.out.println("╰╭╮╯╰╭╮╯ O ");
+System.out.println("╭╰╯╮╭╰╯╮ D ");
+System.out.println("┗╯╰┛┗╯╰┛  ");
+System.out.println("🌷🌹🍃🌹🌷 M ");
+System.out.println("┏╮╭┓┏╮╭┓  O ");
+System.out.println("╰╭╮╯╰╭╮╯  R ");
+System.out.println("╭╰╯╮╭╰╯╮  N ");
+System.out.println("┗╯╰┛┗╯╰┛   I ");
+System.out.println("🌷🌹🍃🌹🌷 N ");
+System.out.println("┏╮╭┓┏╮╭┓  G ");
+System.out.println("╰╭╮╯╰╭╮╯   ");
+System.out.println("╭╰╯╮╭╰╯╮  D ");
+System.out.println("┗╯╰┛┗╯╰┛  E ");
+System.out.println("🌷🌹🍃🌹🌷 A ");
+System.out.println("      FRIEND      ");
+	}
+}
